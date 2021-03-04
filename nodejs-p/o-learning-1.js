ec8178924122683a0166ccb81a40b6f6e24d5c0b
Jon Norris <jon_norris@apple.com>
2021-03-04 13:45:46 -0800

    Initial commit

---
+++ nodejs-p/o-learning-1.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+let answer = 10 * "woop";
+let nums = 0.1 + 0.7;
+console.log(chalk.red(`the answer is: ${nums}`));
+console.log(`and: ${answer}`);
+console.log(chalk.blue('Bye'));
+// express and koa for creating REST interface [ or hapi ]
+// axios module is great for getting random user data to use in code
+// https://randomuser.me/api
+// npm = NodeJS package manager
